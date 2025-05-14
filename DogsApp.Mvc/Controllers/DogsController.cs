eb903d24f10fb5b1b77f2b3ec51c0463acf2fc87
E-Rx <elise.rochaix@gmail.com>
2025-05-14 13:23:32 +0200

    Controllers added

---
+++ DogsApp.Mvc/Controllers/DogsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DogsApp.Mvc.Models;
+using DogsApp.Mvc.Services;
+
+namespace DogsApp.Mvc.Controllers
+{
+    public class DogsController : Controller
+    {
+        DogServices dogService = new DogServices();
+        
+        [Route("")]
+        public IActionResult Index()
+        {
+            var dogs = dogService.GetAllDogs();
+            return View(dogs);
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create(Dog dog)
+        {
+            dogService.AddDog(dog);
+            return RedirectToAction(nameof(Index),
+            new { id = dog.Id });
+
+
+        }
+    }
+}
