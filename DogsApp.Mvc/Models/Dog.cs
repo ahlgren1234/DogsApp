b19b20806ac5b28522ba1afafbae9efb8295f530
Peter Ahlgren <ahlgren1234@gmail.com>
2025-05-14 11:46:40 +0200

    Add model

---
+++ DogsApp.Mvc/Models/Dog.cs
@@ -0,0 +1,9 @@
+﻿namespace DogsApp.Mvc.Models
+{
+    public class Dog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int Age { get; set; }
+    }
+}
